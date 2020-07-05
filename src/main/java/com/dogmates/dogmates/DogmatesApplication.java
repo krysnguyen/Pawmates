c03e655c2bd54ed88347acde3641fd31dc4f93d8
cmalla <cmalla@sfu.ca>
2020-07-05 13:02:40 -0700

    Initial spring boot project setup

---
+++ src/main/java/com/dogmates/dogmates/DogmatesApplication.java
@@ -0,0 +1,13 @@
+package com.dogmates.dogmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogmatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogmatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogmates/dogmates/DogmatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogmates.dogmates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogmatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
