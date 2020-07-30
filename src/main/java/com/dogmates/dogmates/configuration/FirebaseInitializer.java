cf97e4ff4abc617b046c8986a17a217f4f47999f
Chad Malla <chad.malla@sbisecsol.com>
2020-07-29 18:23:16 -0700

    add back the serviceAccount variable

--- src/main/java/com/dogmates/dogmates/configuration/FirebaseInitializer.java
+++ src/main/java/com/dogmates/dogmates/configuration/FirebaseInitializer.java
@@ -3,6 +3,7 @@ package com.dogmates.dogmates.configuration;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,9 @@ public class FirebaseInitializer {
 
     @PostConstruct
     public void initialize() throws IOException {
+        
+        InputStream serviceAccount = getClass().getResourceAsStream("/service_acc_firebase.json");
+
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(fromStream(serviceAccount))
                 .setDatabaseUrl("https://pawmates-71be7.firebaseio.com")
