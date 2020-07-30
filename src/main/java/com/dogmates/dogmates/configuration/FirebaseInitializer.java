b013712d1cf428ff00853340728329906839fcd8
Chad Malla <chad.malla@sbisecsol.com>
2020-07-29 17:57:09 -0700

    delete print statements

--- src/main/java/com/dogmates/dogmates/configuration/FirebaseInitializer.java
+++ src/main/java/com/dogmates/dogmates/configuration/FirebaseInitializer.java
@@ -1,16 +1,11 @@
 package com.dogmates.dogmates.configuration;
 
-import com.dogmates.dogmates.PawMatesApplication;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import java.io.IOException;
+import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
 
 import static com.google.auth.oauth2.GoogleCredentials.fromStream;
 
@@ -19,12 +14,6 @@ public class FirebaseInitializer {
 
     @PostConstruct
     public void initialize() throws IOException {
-        InputStream serviceAccount = getClass().getResourceAsStream("/service_acc_firebase.json");
-        InputStream inputStream = PawMatesApplication.class.getResourceAsStream("/service_acc_firebase.json");
-        String result = new BufferedReader(new InputStreamReader(inputStream)).lines()
-                .parallel().collect(Collectors.joining("\n"));
-        System.out.println("RESULT: +++++ " + result);
-
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(fromStream(serviceAccount))
                 .setDatabaseUrl("https://pawmates-71be7.firebaseio.com")
