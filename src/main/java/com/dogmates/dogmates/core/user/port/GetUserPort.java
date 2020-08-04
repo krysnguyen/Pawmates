5382c0b68bad5a620bbd2399ac91e74a2e2ab0e7
Chad Malla <chad.malla@sbisecsol.com>
2020-08-04 16:45:11 -0700

    update where the loading is set to false

--- src/main/java/com/dogmates/dogmates/core/user/port/GetUserPort.java
+++ src/main/java/com/dogmates/dogmates/core/user/port/GetUserPort.java
@@ -10,6 +10,8 @@ public interface GetUserPort {
      * Get all the users from the DB
      *
      * @return A list of users
+     * @throws ExecutionException
+     * @throws InterruptedException
      */
     List<User> getAll() throws ExecutionException, InterruptedException;
 
@@ -18,6 +20,19 @@ public interface GetUserPort {
      *
      * @param id The id of the user to get
      * @return The user from the DB
+     * @throws ExecutionException
+     * @throws InterruptedException
      */
     User getUser(String id) throws ExecutionException, InterruptedException;
+
+    /**
+     * Get users to match with after filtering.
+     *
+     * @param userId The ID of the user to get potential matches for.
+     * @return The list of potential users to match with.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    List<User> getPotentialMatches(String userId) throws ExecutionException, InterruptedException;
+
 }
--- src/main/java/com/dogmates/dogmates/db/user/GetUserAdapter.java
+++ src/main/java/com/dogmates/dogmates/db/user/GetUserAdapter.java
@@ -4,9 +4,10 @@ import com.dogmates.dogmates.core.user.domain.User;
 import com.dogmates.dogmates.core.user.port.GetUserPort;
 import com.google.cloud.firestore.Firestore;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 import static java.lang.String.format;
@@ -34,5 +35,41 @@ public class GetUserAdapter implements GetUserPort {
                 .get()
                 .get()
                 .toObject(User.class);
+    }
+
+    @Override
+    public List<User> getPotentialMatches(String userId) throws ExecutionException, InterruptedException {
+        val user = firestore.document(format(USER_PATH, userId))
+                .get()
+                .get()
+                .toObject(User.class);
+
+        val allIds = firestore.collection("users")
+                .get()
+                .get()
+                .getDocuments()
+                .stream()
+                .map(user1 -> user1.toObject(User.class))
+                .map(User::getId)
+                .collect(toList());
+
+        val filterBy = new HashSet<String>();
+        filterBy.addAll(user.getLiked());
+        filterBy.addAll(user.getDisliked());
+        filterBy.addAll(user.getMatches());
+        filterBy.add(userId);
+
+        val allIdsSet = new HashSet<String>();
+        allIdsSet.addAll(allIds);
+
+        allIdsSet.removeAll(filterBy);
+
+        val ids = new ArrayList<>(allIdsSet);
+
+        return firestore.collection("users")
+                .whereIn("id", ids)
+                .get()
+                .get()
+                .toObjects(User.class);
     }
 }
