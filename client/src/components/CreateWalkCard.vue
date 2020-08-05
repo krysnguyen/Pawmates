bb2d04bd7915c57e6889bbdc5dc61f9909d5b701
mal24 <michael_lam_7@sfu.ca>
2020-08-04 22:59:49 -0700

    adjusted create walk card

--- client/src/components/CreateWalkCard.vue
+++ client/src/components/CreateWalkCard.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="create-walk-card">
-    <b-card class="mb-3">
-      <b-card-title title="Create a walk" class="mb-0"></b-card-title>
+    <b-card no-body class="overflow-hidden mb-3" style="max-height: 150px;">
       <b-link to="/createwalk" class="stretched-link card-link">
+        <b-row class="row-in-card" no-gutters>
+          <b-col cols="2" align-self="center">
+            <b-card-img src="https://placedog.net/150/150?id=15" alt="Profile image" class="rounded-circle" style="max-height: 120px; max-width: 120px"></b-card-img>
+          </b-col>
+          <b-col cols="10" align-self="center">
+            <b-card-title class="mb-0" title="Create a walk">
+            </b-card-title>
+            <b-card-sub-title sub-title="(should have user's image to the left)"></b-card-sub-title>
+          </b-col>
+        </b-row>
       </b-link>
     </b-card>
   </div>
@@ -15,6 +24,11 @@
 </script>
 
 <style lang="scss" scoped>
+  
+  .create-walk-card .card-title {
+    font-size: 1.75rem;
+  }
+  
   .card {
     box-shadow: 0px 0px 15px #bbb;
   }
